f983df8d6ced0004a61068ce89db31d4599394f9
lester <lieyunwuhen@sina.com>
2020-06-02 13:45:27 +0800

    init

---
+++ static/index.js
@@ -0,0 +1,130 @@
+$(function () {
+  $("#xml")
+    .bind("input propertychange", function () {
+      sessionStorage.setItem("xml", $(this).val());
+    })
+    .val(sessionStorage.getItem("xml"));
+
+  $("#run").click(function () {
+    var xml = $("#xml").val();
+    // console.log(xml);
+
+    const $xml = $(xml);
+
+    const jobrels = [];
+    const jobinfos = [];
+
+    const excludeJobs = ["begin", "agentid", "timingplan", "end"];
+
+    function loop($els, prevJobs = [], isSerial = false) {
+      console.log($els, $els.length);
+      let latestJobs = [];
+      for (let i = 0; i < $els.length; i++) {
+        const el = $els[i];
+        const nodename = $.trim(
+          (el.nodename || el.tagName || "").toLowerCase()
+        );
+        if (nodename.substr(0, 1) == "#") continue;
+        if (nodename == "text") continue;
+        if (!nodename || nodename == "name") continue;
+        if (nodename == "serial") {
+          const jobs = loop($(el).children(), prevJobs, true);
+          latestJobs = isSerial ? jobs : latestJobs.concat(jobs);
+          if (isSerial) prevJobs = jobs;
+          continue;
+        }
+        if (nodename == "parallel") {
+          const jobs = loop($(el).children(), prevJobs, false);
+          if (isSerial) prevJobs = jobs;
+          latestJobs = isSerial ? jobs : latestJobs.concat(jobs);
+          continue;
+        }
+        if (excludeJobs.includes(nodename)) continue;
+
+        const job = { name: `job_gen_${jobinfos.length}` };
+        job.typename = nodename;
+
+        jobinfos.push(job);
+        console.log(job, i);
+
+        const $attrs = $(el).children();
+        for (let j = 0; j < $attrs.length; j++) {
+          const $attr = $attrs[j];
+          job.prevJobs = prevJobs;
+          if ($attr.tagName)
+            job[$attr.tagName.toLowerCase()] = $.trim($attr.innerHTML);
+        }
+
+        if (isSerial) {
+          prevJobs = latestJobs = [job.name];
+        } else {
+          latestJobs.push(job.name);
+        }
+      }
+
+      return latestJobs;
+    }
+
+    loop($xml);
+    const props = "name,typename,progname,para,exppara,jobdesc,autorun,prevshell,nextshell,agentid,hostuser,datetype,period,timingplan,lean,ostr,errdelay,ignoreerr,maxnum,cyclecount,cycleinterval,cyclebreak,issplit,splitcount,priority,timeout,virresource,condition,successv,warnningv,errorv,failedv,monititle".split(
+      ","
+    );
+    const infoCsv = `${props.join(",")}${jobinfos
+      .map((job) => {
+        return props
+          .map((prop) => (typeof job[prop] == "undefined" ? "" : job[prop]))
+          .join(",");
+      })
+      .join("\n")}
+    `;
+
+    $("#jobinfo").val(infoCsv);
+
+    const relCsv = `jobname,prejob\n${jobinfos
+      .reduce((array, job) => {
+        const jobname = job.name;
+        const prevJobs = job.prevJobs || [];
+        prevJobs.forEach((prevjob) => {
+          array.push(`${jobname},${prevjob}`);
+        });
+        // 排除依赖关系
+        const leans = job.lean ? job.lean.split(',') : [];
+        leans.forEach((prevjob) => {
+          prevjob = $.trim(prevjob);
+          if (!prevjob) return;
+          array.push(`${jobname},${prevjob}`);
+        })
+        return array;
+      }, [])
+      .join("\n")}
+    `;
+
+    $("#jobrel").val(relCsv);
+  });
+
+  $("#btn-csv-info").click(function () {
+    $("#run").click();
+    exportCsv("taskctl_import_jobinfo.csv", $("#jobinfo").val());
+  });
+  $("#btn-csv-rela").click(function () {
+    $("#run").click();
+    exportCsv("taskctl_import_jobrela.csv", $("#jobrel").val());
+  });
+});
+
+function exportCsv(name, data) {
+  var blob = new Blob([data]);
+
+  if (window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveBlob(blob, name);
+  } else {
+    var a = window.document.createElement("a");
+    a.href = window.URL.createObjectURL(blob, {
+      type: "text/csv",
+    });
+    a.download = name;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+}
